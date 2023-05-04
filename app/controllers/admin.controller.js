dc0451e58a857d4ffc8557eeebfa735aeeeb03ab
insmelm <sagar.melmatti@vanderlande.com>
2023-05-04 20:50:50 +0530

    send email on update complaint status

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -6,7 +6,13 @@ const User = db.user;
 const Op = db.Sequelize.Op;
 const pdf = require('html-pdf');
 const pdfTemplate = require('../documents');
+const nodemailer = require('nodemailer');
 const sequelize = require("sequelize");
+const email_config = require("../config/email.json");
+
+var sender_email = email_config.sender_email;
+var sender_password = email_config.sender_password;
+var admin_email = email_config.admin_email;
 
 // Retrieve all Payments from the database.
 exports.findAllUser = (req, res) => {
@@ -260,7 +266,7 @@ exports.findComplaintByUserId = (req, res) => {
 };
 
 // Update a Complaint by the id in the request
-exports.updateComplaintStatus = (req, res) => {
+exports.updateComplaintStatus = async (req, res) => {
   const id = req.params.id;
   // Create a Complaint
   const complaint = {
@@ -269,25 +275,84 @@ exports.updateComplaintStatus = (req, res) => {
     complaint_resolved_date: new Date()
   };
 
-  Complaint.update(complaint, {
-    where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: "Complaint was updated successfully.",
-        });
-      } else {
-        res.send({
-          message: `Cannot update Complaint with id=${id}. Maybe Complaint was not found or req.body is empty!`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: "Error updating Complaint with id=" + id,
-      });
+  //try {
+    const complaint_status = await Complaint.update(complaint, {
+      where: { id: id },
     });
+
+    const complaint_result = await Complaint.findByPk(req.params.id);
+    const location_result = await Location.findByPk(complaint_result.locationId);
+    const user_result = await User.findByPk(complaint_result.userId);
+
+    Complaint.findByPk(id)
+      .then((complaint_data) => {
+        if (complaint_data) {
+
+          const transporter = nodemailer.createTransport({
+            host: 'us2.smtp.mailhostbox.com',
+            port: 25,
+            auth: {
+              user: sender_email,
+              pass: sender_password,
+            },
+          }); transporter.verify().then(console.log).catch(console.error);
+
+          var usermailOptions = {
+            from: sender_email,
+            to: user_result.email,
+            subject: 'Complaint Change Status',
+            text: 'Complaint Change Status', // plain text body
+            html: '</br><SPAN STYLE="font-size:12.0pt"> <b>Dear ' + capitalizeFirstLetter(user_result.name) + ' </b></span>, </br></br> <SPAN STYLE="font-size:13.0pt"> Your Complaint status has been chnaged </br>' +
+              '<p> Complaint details mentioned below : <p>' +
+              ' Complaint Status: ' + complaint.status +
+              ' <br>Comment : ' + complaint.comment + '',
+          };
+
+          transporter.sendMail(usermailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+
+          var mailOptionsLocationHead = {
+            from: sender_email,
+            to: location_result.email,
+            subject: 'Complaint Change Status',
+            text: 'Complaint Change Status', // plain text body
+            html: '</br><SPAN STYLE="font-size:12.0pt"> <b>Dear ' + capitalizeFirstLetter(location_result.headName) + ' </b></span>, </br></br> <SPAN STYLE="font-size:13.0pt"> Complaint status has been changed for your location, </br> ' +
+              '<p> Details mentioned below: <p>' +
+              ' User Name: ' + user_result.name +
+              ' <br> Email : ' + user_result.email +
+              ' <br> Complaint In Short : ' + complaint_result.title +
+              ' <br> Complaint Description: ' + complaint_result.description +
+              ' <br> Complaint Status : ' + complaint.status +
+              ' <br> Comment : ' + complaint.comment + '',
+          };
+
+          transporter.sendMail(mailOptionsLocationHead, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+
+          res.send("Complaint Status successfully.");
+        } else {
+          res.status(404).send({
+            message: `Cannot find User with id=${id}.`,
+          });
+        }
+      })
+
+      /*
+  } catch (error) {
+    console.log('error');
+    return 'test';
+  }
+  */
 
 };
 
@@ -478,9 +543,13 @@ exports.createComplaintReport = async (req, res) => {
 
 };
 
-
-
 // Retrieve all Students from the database.
 exports.fetchComplaintReport = (req, res) => {
   res.sendFile(`${__dirname}/result.pdf`)
 };
+
+function capitalizeFirstLetter(str) {
+  // converting first letter to uppercase
+  const capitalized = str.replace(/^./, str[0].toUpperCase());
+  return capitalized;
+}
