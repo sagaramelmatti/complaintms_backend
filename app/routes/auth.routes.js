7d5b7cd8f205066c71663f33f6e642b5e3aa9b9e
IT_HelpDesk <sagaramelmatti2gmail.com>
2023-06-05 18:24:05 +0530

    allllow origina all

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,12 +2,12 @@ const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept");
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next();
-    });
+      });
 
     app.post("/api/auth/signup",
         [
